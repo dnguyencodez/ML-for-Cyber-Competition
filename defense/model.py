76fa59f34494e2b5ffbc948106e83e346a6416a7
dnguyencodez <dylan.nguyen39@gmail.com>
2024-04-09 12:25:28 -0500

    updated model constructor

--- defense/model.py
+++ defense/model.py
@@ -1,8 +1,14 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_selection import mutual_info_classif, SelectKBest
+from sklearn.decomposition import PCA
 
 class malware_detection_model():
 
     def __init__(self,
                  classifier=RandomForestClassifier(),
+                 textual_extractor = CountVectorizer(),
                  ) -> None:
-        pass
+        
+        self.classifier = classifier
+        self.textual_extractor = textual_extractor
